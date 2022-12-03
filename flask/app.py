d3112de2c3fbc0ef07ad7e16b32fb9c09ffd2730
walter.hwang <lyvius2@naver.com>
2022-12-03 22:02:57 +0900

    템플릿 엔진 적용, 데이터 표시

--- flask/app.py
+++ flask/app.py
@@ -2,23 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+console_data = {
+    1: {'name': '패밀리컴퓨터', 'company': 'Nintendo', 'topTitle': {'드래곤퀘스트3': 1987, '파이널판타지3': 1990, '패미컴탐정클럽 Part2': 1989}},
+    2: {'name': 'Play Station2', 'company': 'SONY', 'topTitle': {'바이오하자드4': 2006, '령 제로': 2001}}
+}
+
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html', consoles=console_data)
 
 
-@app.route('/home')
-def home():
-    return '''
-    <h1>제목</h1>
-    <p>본문</p>
-    '''
-
-
-@app.route('/user/<user_name>/<int:user_id>')
-def user(user_name, user_id):
-    return f'Hello, {user_name}({user_id})!'
+@app.route('/console/<int:id>')
+def console(id):
+    return render_template('console.html',
+                           console_name=console_data[id]['name'],
+                           console_title=console_data[id]['topTitle'])
 
 
 if __name__ == '__main__':
