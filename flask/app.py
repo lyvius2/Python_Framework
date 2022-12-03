5c6188a7ba6eeb8d8a345d02d1ee33253c42f906
walter.hwang <lyvius2@naver.com>
2022-12-03 18:36:56 +0900

    flask app 초기 commit

---
+++ flask/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return '''
+    <h1>제목</h1>
+    <p>본문</p>
+    '''
+
+
+@app.route('/user/<user_name>/<int:user_id>')
+def user(user_name, user_id):
+    return f'Hello, {user_name}({user_id})!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
