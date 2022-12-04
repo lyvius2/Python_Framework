0dc27edc857044f9cd372543640dffe25a01091c
walter.hwang <lyvius2@naver.com>
2022-12-04 17:16:15 +0900

    주석 추가

--- fast_api/database.py
+++ fast_api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = '{DB_URL}'
+DB_URL = '{DB_URL}'  # DB Connection 주소를 넣으세요.
 
 engine = create_engine(DB_URL, encoding='utf-8')
 
